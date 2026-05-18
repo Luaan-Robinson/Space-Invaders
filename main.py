3b0d9b5cf36a1641aae3e858da114d1eacf28245
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-05-18 16:06:35 +0200

    player movement

--- main.py
+++ main.py
@@ -26,12 +26,29 @@ YELLOW_LASER = pygame.image.load(os.path.join('assets', 'pixel_laser_yellow.png'
 
 BG = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'background-black.png')), (WIDTH,HEIGHT))
 
+# abstract class
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.laser = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, "red", (self.x, self.y, 50, 50))
+
+
 def main():
     run = True
     FPS = 60
     level = 1
     lives = 5
-    main_font = pygame.font.SysFont('lexend', 50)
+    main_font = pygame.font.SysFont('arial', 50)
+    player_vel = 5
+    ship = Ship(300, 650)
 
 
     clock = pygame.time.Clock()
@@ -45,6 +62,8 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        ship.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -54,6 +73,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            ship.x -= player_vel   
+        if keys[pygame.K_RIGHT]:
+            ship.x += player_vel
+        if keys[pygame.K_UP]:
+            ship.y -= player_vel
+        if keys[pygame.K_DOWN]:
+            ship.y += player_vel                                          
 
 
 main()            
