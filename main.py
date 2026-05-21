de9b7e55594e86ad25e8e13c85793cd7909eb151
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-05-21 12:44:47 +0200

    ship collisions

--- main.py
+++ main.py
@@ -232,7 +232,10 @@ def main():
             if random.randrange(0, 2*60) == 1:
                 enemy.shoot()
 
-            if enemy.y + enemy.get_height() > HEIGHT:
+            if collide(enemy, player):
+                player.health -= 10
+                enemies.remove(enemy)        
+            elif enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
 
